01c288e37713c3fc167ead0c3592e674c76fe4e9
Diana <diana38974@gmail.com>
2023-08-03 13:59:14 -0400

    deleted comments.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,18 +1,3 @@
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
-
-// const HomeScreen = () => {
-//   return (
-//     <View>
-//       <Text>Home Screen</Text>
-//     </View>
-//   )
-// }
-
-// export default HomeScreen;
-
-// const styles = StyleSheet.create({})
-
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
